94765ebb528440ba8b3e9f53749f116b13eb04f5
joncrall <erotemic@gmail.com>
2020-01-22 15:04:03 -0500

    py27 fix

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -110,7 +110,7 @@ autoapi_modules = {
 
 
 autoapi_dirs = ['../../kwarray']
-autoapi_keep_files = True
+# autoapi_keep_files = True
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
