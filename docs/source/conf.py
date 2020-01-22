67e6982a96cbb6eed02056b646eb117f16adf172
joncrall <erotemic@gmail.com>
2020-01-22 13:59:58 -0500

    wip

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -89,6 +89,7 @@ version = '.'.join(release.split('.')[0:2])
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    'autoapi.extension',
     'sphinx.ext.autodoc',
     'sphinx.ext.viewcode',
     'sphinx.ext.intersphinx',
@@ -96,6 +97,8 @@ extensions = [
     'sphinx.ext.napoleon',
     'sphinx.ext.autosummary',
 ]
+
+autoapi_dirs = ['../../kwarray']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
