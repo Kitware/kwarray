ea516a1815e4b3050adce97e999f135eb7bc092a
joncrall <erotemic@gmail.com>
2023-07-13 12:59:24 -0400

    Fix test errors

--- kwarray/util_groups.py
+++ kwarray/util_groups.py
@@ -72,13 +72,16 @@ def group_items(item_list, groupid_list, assume_sorted=False, axis=None):
 
     Example:
         >>> from kwarray.util_groups import *  # NOQA
-        >>> items = np.array([0, 1, 2, 3, 4, 5, 6, 7])
-        >>> keys = np.array( [2, 2, 1, 1, 0, 1, 0, 1])
+        >>> items = np.array([0, 1, 2, 3, 4, 5, 6, 7, 1, 1])
+        >>> keys = np.array( [2, 2, 1, 1, 0, 1, 0, 1, 1, 1])
         >>> grouped = group_items(items, keys)
-        >>> print(ub.urepr(grouped, nl=1, with_dtype=False))
-        {
+        >>> # Different versions of numpy may produce different orderings
+        >>> # so normalize these to make test output consistent
+        >>> [gxs.sort() for gxs in grouped.values()]
+        >>> print('grouped = ' + ub.urepr(grouped, nl=1, with_dtype=False, sort=1))
+        grouped = {
             0: np.array([4, 6]),
-            1: np.array([2, 3, 5, 7]),
+            1: np.array([1, 1, 2, 3, 5, 7]),
             2: np.array([0, 1]),
         }
     """
@@ -130,10 +133,10 @@ def group_indices(idx_to_groupid, assume_sorted=False):
         >>> import ubelt as ub
         >>> idx_to_groupid = np.array([2, 1, 2, 1, 2, 1, 2, 3, 3, 3, 3])
         >>> (keys, groupxs) = group_indices(idx_to_groupid)
-        >>> print(ub.urepr(keys, with_dtype=False))
-        >>> print(ub.urepr(groupxs, with_dtype=False))
-        np.array([1, 2, 3])
-        [
+        >>> print('keys = ' + ub.urepr(keys, with_dtype=False))
+        >>> print('groupxs = ' + ub.urepr(groupxs, with_dtype=False))
+        keys = np.array([1, 2, 3])
+        groupxs = [
             np.array([1, 3, 5]),
             np.array([0, 2, 4, 6]),
             np.array([ 7,  8,  9, 10]),
@@ -147,10 +150,13 @@ def group_indices(idx_to_groupid, assume_sorted=False):
         >>> # 2d arrays must be flattened before coming into this function so
         >>> # information is on the last axis
         >>> (keys, groupxs) = group_indices(idx_to_groupid.T[0])
-        >>> print(ub.urepr(keys, with_dtype=False))
-        >>> print(ub.urepr(groupxs, with_dtype=False))
-        np.array([ 24, 129, 659, 822])
-        [
+        >>> # Different versions of numpy may produce different orderings
+        >>> # so normalize these to make test output consistent
+        >>> [gxs.sort() for gxs in groupxs]
+        >>> print('keys = ' + ub.urepr(keys, with_dtype=False))
+        >>> print('groupxs = ' + ub.urepr(groupxs, with_dtype=False))
+        keys = np.array([ 24, 129, 659, 822])
+        groupxs = [
             np.array([ 0,  4, 10]),
             np.array([1]),
             np.array([2, 3, 5, 6, 8, 9]),
